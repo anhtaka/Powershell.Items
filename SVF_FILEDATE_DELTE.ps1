0a46dbb109bf3f24dd2604796a1f5c2d2e83bb97
tsuy takayanagi <ponta_gogo677@yahoo.co.jp>
2015-08-13 16:48:30 +0900

    d

---
+++ SVF_FILEDATE_DELTE.ps1
@@ -0,0 +1,26 @@
+﻿#SVFのデータファイルを作成から５時間以上経過したものを削除する。 
+#事前にSet-ExecutionPolicy RemoteSignedを実行する。一度だけ。
+
+$a = Get-ChildItem D:\GRANDIT\CROWN\SVF\DATAFILE\CTEC_?_R*.csv 
+
+foreach($x in $a)  
+{  
+    $xtime = $x.LastWriteTime.AddHours(5)      #5hour set
+    #$x1 = $x.LastWriteTime.Minute(5)
+    if ((Get-Date) -ge $xtime){
+        #{$x.Delete()}  
+        Remove-Item $x
+    }
+} 
+
+    
+$a1 = Get-ChildItem D:\GRANDIT\CROWN\SVF\DATAFILE\CROW_?_R*.csv 
+foreach($x1 in $a1)  
+{  
+    $xtime1 = $x1.LastWriteTime.AddHours(5)    #5hour set
+    #$x1 = $x.LastWriteTime.Minute(5)
+    if ((Get-Date) -ge $xtime1){
+        #{$x.Delete()}  
+        Remove-Item $x1
+    }
+} 
